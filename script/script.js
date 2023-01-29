8119181d1327389ac29e0d15e6d7d64813a68423
FernandoM87 <101877869+FernandoM87@users.noreply.github.com>
2023-01-29 21:34:02 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,11 @@
+/* loadJSON (URL, callback);
+                                        then() <-- fulfilled
+                                        catch() <--- rejected
+
+let promise =  fetch(URL); 
+
+1- pending
+2- fulfilled
+3- rejected
+*/
+
